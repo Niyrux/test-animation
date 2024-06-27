9261404c828d46e76608a6b737e05a18c6805172
Niyrux <97093768+Niyrux@users.noreply.github.com>
2024-06-27 11:43:03 +0200

    animation aimant

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
-var projects = document.querySelectorAll('.box');
-
-var options = {
-    threshold: 0.7
-};
-
-var observer = new IntersectionObserver(function (entries, observer) {
-    entries.forEach(function (entry) {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('active');
-
-            var topOffset = entry.target.getBoundingClientRect().top + window.pageYOffset;
-
-            window.scrollTo({
-                top: topOffset,
-                behavior: 'smooth'
-            });
-        } else {
-            entry.target.classList.remove('active');
-        }
+document.addEventListener("DOMContentLoaded", function () {
+    // Registrez ScrollTrigger avec GSAP
+    gsap.registerPlugin(ScrollTrigger);
+    // Sélectionnez tous les paires de .box et .test
+    gsap.from(".test1", {
+        scrollTrigger: {
+            trigger: ".box1",
+            scrub: true,
+            pin: true,
+            start: "top top",
+            end: "+=100%"
+        },
+        scaleX: 0,
+        transformOrigin: "left center",
+        ease: "none"
     });
-}, options);
+    gsap.from(".test2", {
+        scrollTrigger: {
+            trigger: ".box2",
+            scrub: true,
+            pin: true,
+            start: "top top",
+            end: "+=100%"
+        },
+        scaleX: 0,
+        transformOrigin: "left center",
+        ease: "none"
+    });
+    gsap.from(".test3", {
+        scrollTrigger: {
+            trigger: ".box3",
+            scrub: true,
+            pin: true,
+            start: "top top",
+            end: "+=100%"
+        },
+        scaleX: 0,
+        transformOrigin: "left center",
+        ease: "none"
+    });
+    gsap.from(".test4", {
+        scrollTrigger: {
+            trigger: ".box4",
+            scrub: true,
+            pin: true,
+            start: "top top",
+            end: "+=100%"
+        },
+        scaleX: 0,
+        transformOrigin: "left center",
+        ease: "none"
+    });
 
-projects.forEach(function (project) {
-    observer.observe(project);
-});
+})
